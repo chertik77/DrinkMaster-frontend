640588b854b57970b6e506f204c9f9f1b8a5cb67
chertik77 <babychdenya@gmail.com>
2024-04-26 22:52:58 +0100

    style: clean up all files

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,10 @@
 import { ThemeProvider } from 'next-themes'
 
-import { userService } from '@/services/user.service'
-
 const Page = async () => {
-  const { userTheme } = await userService.getUserProfile()
-
   return (
     <ThemeProvider
       attribute='class'
-      enableSystem={false}
-      defaultTheme={userTheme}>
+      enableSystem={false}>
       <div>d32d32</div>
     </ThemeProvider>
   )
