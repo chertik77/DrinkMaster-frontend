633ddf1d2ecb32587f070ae14e5cb82bafab33bb
chertik77 <denysbabych.dev@gmail.com>
2024-06-22 14:48:47 +0100

    build: fix sigterm vercel build error

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   compress: true,
   images: { remotePatterns: [{ hostname: 'res.cloudinary.com' }] },
-  env: { API_BASE_URL: process.env.API_BASE_URL }
+  env: { API_BASE_URL: process.env.API_BASE_URL },
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  }
 }
 
 export default nextConfig
